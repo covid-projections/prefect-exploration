015a9417dbfadbd356d924f53f680f30fea99837
Christopher Lin <powertothepenguins@gmail.com>
2022-01-13 15:09:20 -0800

    Create per-state flows for updating location Parquet files

--- update_location_parquet_files.py
+++ update_location_parquet_files.py
@@ -14,13 +14,18 @@ from google.cloud import storage
 from prefect import flatten, Flow, task, unmapped
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import UniversalRun
-from prefect.tasks.shell import ShellTask
+from prefect.storage import GCS
+from prefect.tasks.prefect import create_flow_run
 from prefect.tasks.secrets import EnvVarSecret
+from typing import List
 
-DATA_PATH = pathlib.Path(os.environ["DATAPATH"]) / "final"
-DATA_PATH.mkdir(parents=True, exist_ok=True)
-FILENAME_PREFIX = "can_scrape_api_covid_us"
+GEO_DATA_PATH = "https://media.githubusercontent.com/media/covid-projections/covid-data-model/main/data/geo-data.csv"
 
+@task
+def location_ids_for(state: str, geo_data_path: str = GEO_DATA_PATH) -> List[str]:
+    df = pd.read_csv(geo_data_path)
+    df = df[df["state"] == state]
+    return df["location_id"].tolist()
 
 @task
 def fetch_location_ids(connstr: str):
@@ -58,46 +63,68 @@ def create_location_parquet(connstr: str, location_id: str):
         # a dependency? Would probably use pyarrow instead for Parquet IO.
         df = pd.read_sql_query(query, conn)
 
+    data_dir = pathlib.Path('/home/clizzin/prefect-exploration/can-scrape-outputs')
+    data_dir.mkdir(parents=True, exist_ok=True)
+    filename_prefix = "can_scrape_api_covid_us"
+
     # Write vintage file.
     ts = prefect.context.scheduled_start_time
     dt_str = pd.to_datetime(ts).strftime("%Y-%m-%dT%H")
-    vintage_fn = f"{FILENAME_PREFIX}_{location_id}_{dt_str}.parquet"
-    df.to_parquet(DATA_PATH / vintage_fn, index=False)
+    vintage_fn = f"{filename_prefix}_{location_id}_{dt_str}.parquet"
+    vintage_path = data_dir / vintage_fn
+    df.to_parquet(vintage_path, index=False)
     logger.info(vintage_fn)
 
     # Replace primary file.
-    fn = f"{FILENAME_PREFIX}_{location_id}.parquet"
-    df.to_parquet(DATA_PATH / fn, index=False)
+    fn = f"{filename_prefix}_{location_id}.parquet"
+    path = data_dir / fn
+    df.to_parquet(path, index=False)
     logger.info(fn)
 
     # Upload to Google Cloud Storage
     storage_client = storage.Client()
     bucket = storage_client.bucket("prefect-exploration")
-    upload(fn, bucket, f"location-parquet-files/{fn}")
-    upload(vintage_fn, bucket, f"location-parquet-files/{vintage_fn}")
+    upload(path, bucket, f"location-parquet-files/{fn}")
+    upload(vintage_path, bucket, f"location-parquet-files/{vintage_fn}")
 
     return vintage_fn, fn
 
 
-@task
-def get_gcs_cmd(fn):
-    return f"gsutil acl ch -u AllUsers:R gs://can-scrape-outputs/final/{fn}"
 
+def create_flow(state):
+    with Flow(f"update-location-parquet-files ({state})",
+              storage=GCS(bucket="prefect-flows")) as flow:
 
-def main():
-    with Flow("update-location-parquet-files", executor=LocalDaskExecutor()) as flow:
-        connstr = EnvVarSecret("COVID_DB_CONN_URI")
-        location_ids = fetch_location_ids(connstr)
+        location_ids = location_ids_for(state, GEO_DATA_PATH)
         filename_tuples = create_location_parquet.map(
             connstr=unmapped(connstr), location_id=location_ids
         )
+    flow.run_config = UniversalRun(labels=["dev"])
+    return flow
 
-        file_permission_commands = get_gcs_cmd.map(flatten(filename_tuples))
-        # TODO: figure out how to test this
-        # ShellTask().map(file_permission_commands)
 
-    flow.run_config = UniversalRun()
-    flow.register(project_name="prefect-exploration")
+def main():
+    df = pd.read_csv(GEO_DATA_PATH)
+    states = df["state"].dropna().unique().tolist()
+
+    # Create one flow for each state.
+
+    flow_ids = []
+    for state in states:
+        flow = create_flow(state)
+        flow_id = flow.register(project_name="prefect-exploration")
+        flow_ids.append(flow_id)
+
+    # Register a parent flow that runs every state's flow.
+
+    with Flow("update-location-parquet-files",
+              storage=GCS(bucket="prefect-flows")) as parent_flow:
+
+        for flow_id in flow_ids:
+            create_flow_run(flow_id)
+
+    parent_flow.run_config = UniversalRun(labels=["dev"])
+    parent_flow.register(project_name="prefect-exploration")
 
 
 if __name__ == "__main__":
